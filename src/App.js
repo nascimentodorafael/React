a480b547f765309b7ed3a9362a0941d5b2e93591
nascimentodorafael <107429969+nascimentodorafael@users.noreply.github.com>
2023-07-31 12:00:53 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Cabecalho from './componentes/Cabecalho'
+import ComponenteDois from './componentes/ComponenteDois'
+import ComponenteTres from './componentes/ComponenteTres'
+import ComponenteQuatro from './componentes/ComponenteQuatro'
+import ComponenteCinco from './componentes/ComponenteCinco'
+import ComponenteSeis from './componentes/ComponenteSeis'
+import ComponenteSete from './componentes/ComponenteSete'
+import P1 from './componentes/P1'                                                                                                                                                                                                                                                                                                                       
+import Voida from './componentes/Voida'
+import Evento from './componentes/evento' 
+function App() {
+     
+    return(
+        
+        <div>        
+            <Cabecalho />
+            <P1 />
+            <nem_eu_sei />
+            <Voida />
+            <ComponenteDois />
+            <ComponenteTres />
+            <ComponenteQuatro />
+            <ComponenteCinco />
+            <ComponenteSeis />
+            <ComponenteSete />     
+            <Evento />                                                                                                                                                                                                                                                                                                                                                                                                                                
+        </div>
+    )
+}
+export default App
